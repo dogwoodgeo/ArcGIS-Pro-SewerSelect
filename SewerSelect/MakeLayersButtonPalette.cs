03765d58154f1fae9343c65053bbf5bed3be7a55
Bradley Jones <bjones@dogwoodgeo.com>
2018-10-25 12:39:10 -0500

    Moved much of the Create Layers code to the Module1.cs as moy own mthods then called the methods in the MakeLayersButtonPalette.cs (original C# code in this file ispresent and commented out.

--- SewerSelect/MakeLayersButtonPalette.cs
+++ SewerSelect/MakeLayersButtonPalette.cs
@@ -21,82 +21,84 @@ namespace SewerSelect
                 var sewerExists = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().Any(s => s.Name == "Sewer Lines");
                 if (mhExists == false && sewerExists == false)
                 {
-                    MessageBox.Show("Manholes & Sewers are missing from map.", "Message");
+                    MessageBox.Show("Manholes & Sewers are missing from map.", "Warning");
                 }
                 else if (mhExists == false && sewerExists)
                 {
-                    MessageBox.Show("Sewer Lines layer is present. \n\nManholes layer is missing from map.", "Message");
+                    MessageBox.Show("Sewer Lines layer is present. \n\nManholes layer is missing from map.", "Warning");
                 }
                 else if (mhExists && sewerExists == false)
                 {
-                    MessageBox.Show("Manholes layer is present. \n\nSewers layer is missing from map.", "Message");
+                    MessageBox.Show("Manholes layer is present. \n\nSewers layer is missing from map.", "Warning");
                 }
 
                 else
                 {
-                    var mh = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Manholes");
-                    var lines = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Sewer Lines");
+                    Module1.MakeSewersLayers(mapView);
 
-                    //Get the selected features from the map.
-                    var mhOIDList = mh.GetSelection().GetObjectIDs();
-                    var mhOID = mh.GetTable().GetDefinition().GetObjectIDField();
-                    var linesOIDList = lines.GetSelection().GetObjectIDs();
-                    var linesOID = lines.GetTable().GetDefinition().GetObjectIDField();
+                    //var mh = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Manholes");
+                    //var lines = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Sewer Lines");
 
-                    if (mhOIDList.Count() == 0 && linesOIDList.Count() == 0)
-                    {
-                        MessageBox.Show("Manholes & Sewers contain no selected features.", "Warning");
-                    }
+                    ////Get the selected features from the map.
+                    //var mhOIDList = mh.GetSelection().GetObjectIDs();
+                    //var mhOID = mh.GetTable().GetDefinition().GetObjectIDField();
+                    //var linesOIDList = lines.GetSelection().GetObjectIDs();
+                    //var linesOID = lines.GetTable().GetDefinition().GetObjectIDField();
 
-                    else if (mhOIDList.Count() == 0 && linesOIDList.Count() > 0)
-                    {
-                        MessageBox.Show("Manholes layer contains no selected features.", "Warning");
-                    }
+                    //if (mhOIDList.Count() == 0 && linesOIDList.Count() == 0)
+                    //{
+                    //    MessageBox.Show("Manholes & Sewers contain no selected features.", "Warning");
+                    //}
 
-                    else if (mhOIDList.Count() > 0 && linesOIDList.Count() == 0)
-                    {
-                        MessageBox.Show("Sewer Lines layer contains no selected features.", "Warning");
-                    }
+                    //else if (mhOIDList.Count() == 0 && linesOIDList.Count() > 0)
+                    //{
+                    //    MessageBox.Show("Manholes layer contains no selected features.", "Warning");
+                    //}
 
-                    else
-                    {
-                        string linesDefQuery = $"{linesOID} in ({string.Join(",", linesOIDList)})";
-                        string linesURL = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.SEWERMAN.SEWERS_VIEW";
+                    //else if (mhOIDList.Count() > 0 && linesOIDList.Count() == 0)
+                    //{
+                    //    MessageBox.Show("Sewer Lines layer contains no selected features.", "Warning");
+                    //}
 
-                        Uri linesURI = new Uri(linesURL);
-                        var linesSelLayer = LayerFactory.Instance.CreateFeatureLayer(linesURI, mapView, 0, "Sewer Lines SELECTION");
+                    //else
+                    //{
+                    //    string linesDefQuery = $"{linesOID} in ({string.Join(",", linesOIDList)})";
+                    //    string linesURL = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.SEWERMAN.SEWERS_VIEW";
 
-                        linesSelLayer.SetDefinitionQuery(linesDefQuery);
+                    //    Uri linesURI = new Uri(linesURL);
+                    //    var linesSelLayer = LayerFactory.Instance.CreateFeatureLayer(linesURI, mapView, 0, "Sewer Lines SELECTION");
 
-                        CIMLineSymbol lineSymbol = SymbolFactory.Instance.ConstructLineSymbol(
-                            ColorFactory.Instance.RedRGB,
-                            3.0,
-                            SimpleLineStyle.Solid
-                            );
+                    //    linesSelLayer.SetDefinitionQuery(linesDefQuery);
 
-                        CIMSimpleRenderer lineRenderer = linesSelLayer.GetRenderer() as CIMSimpleRenderer;
-                        lineRenderer.Symbol = lineSymbol.MakeSymbolReference();
-                        linesSelLayer.SetRenderer(lineRenderer);
+                    //    CIMLineSymbol lineSymbol = SymbolFactory.Instance.ConstructLineSymbol(
+                    //        ColorFactory.Instance.RedRGB,
+                    //        3.0,
+                    //        SimpleLineStyle.Solid
+                    //        );
 
-                        string mhDefQuery = $"{mhOID} in ({string.Join(",", mhOIDList)})";
-                        string mhURL = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.SEWERMAN.MANHOLES_VIEW";
+                    //    CIMSimpleRenderer lineRenderer = linesSelLayer.GetRenderer() as CIMSimpleRenderer;
+                    //    lineRenderer.Symbol = lineSymbol.MakeSymbolReference();
+                    //    linesSelLayer.SetRenderer(lineRenderer);
 
-                        Uri mhURI = new Uri(mhURL);
-                        var mhSelLayer = LayerFactory.Instance.CreateFeatureLayer(mhURI, mapView, 0, "Manholes SELECTION");
+                    //    string mhDefQuery = $"{mhOID} in ({string.Join(",", mhOIDList)})";
+                    //    string mhURL = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.SEWERMAN.MANHOLES_VIEW";
 
-                        mhSelLayer.SetDefinitionQuery(mhDefQuery);
+                    //    Uri mhURI = new Uri(mhURL);
+                    //    var mhSelLayer = LayerFactory.Instance.CreateFeatureLayer(mhURI, mapView, 0, "Manholes SELECTION");
 
-                        CIMPointSymbol pointSymbol = SymbolFactory.Instance.ConstructPointSymbol(
-                            ColorFactory.Instance.GreenRGB,
-                            8.0,
-                            SimpleMarkerStyle.Circle
-                            );
+                    //    mhSelLayer.SetDefinitionQuery(mhDefQuery);
 
-                        CIMSimpleRenderer pointRenderer = mhSelLayer.GetRenderer() as CIMSimpleRenderer;
-                        pointRenderer.Symbol = pointSymbol.MakeSymbolReference();
-                        mhSelLayer.SetRenderer(pointRenderer);
+                    //    CIMPointSymbol pointSymbol = SymbolFactory.Instance.ConstructPointSymbol(
+                    //        ColorFactory.Instance.GreenRGB,
+                    //        8.0,
+                    //        SimpleMarkerStyle.Circle
+                    //        );
 
-                    }
+                    //    CIMSimpleRenderer pointRenderer = mhSelLayer.GetRenderer() as CIMSimpleRenderer;
+                    //    pointRenderer.Symbol = pointSymbol.MakeSymbolReference();
+                    //    mhSelLayer.SetRenderer(pointRenderer);
+
+                    //}
                 }
             });
 
@@ -116,36 +118,37 @@ namespace SewerSelect
 
                 if (linesExists)
                 {
-                    var mh = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Manholes");
+                    Module1.MakeManholesLayer(mapView);
+                    //var mh = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Manholes");
 
-                    //Get the selected features from the map.
-                    var mhOIDList = mh.GetSelection().GetObjectIDs();
-                    var mhOID = mh.GetTable().GetDefinition().GetObjectIDField();
+                    ////Get the selected features from the map.
+                    //var mhOIDList = mh.GetSelection().GetObjectIDs();
+                    //var mhOID = mh.GetTable().GetDefinition().GetObjectIDField();
 
-                    if (mhOIDList.Count() == 0)
-                    {
-                        MessageBox.Show("There are no manholes selected.", "Warning");
-                    }
+                    //if (mhOIDList.Count() == 0)
+                    //{
+                    //    MessageBox.Show("There are no manholes selected.", "Warning");
+                    //}
 
-                    else
-                    {
-                        string defQuery = $"{mhOID} in ({string.Join(",", mhOIDList)})";
-                        string url = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.SEWERMAN.MANHOLES_VIEW";
+                    //else
+                    //{
+                    //    string defQuery = $"{mhOID} in ({string.Join(",", mhOIDList)})";
+                    //    string url = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.SEWERMAN.MANHOLES_VIEW";
 
-                        Uri uri = new Uri(url);
-                        var selectionLayer = LayerFactory.Instance.CreateFeatureLayer(uri, mapView, 0, "Manholes SELECTION");
+                    //    Uri uri = new Uri(url);
+                    //    var selectionLayer = LayerFactory.Instance.CreateFeatureLayer(uri, mapView, 0, "Manholes SELECTION");
 
-                        selectionLayer.SetDefinitionQuery(defQuery);
+                    //    selectionLayer.SetDefinitionQuery(defQuery);
 
-                        CIMPointSymbol pointSymbol = SymbolFactory.Instance.ConstructPointSymbol(
-                            ColorFactory.Instance.GreenRGB,
-                            8.0,
-                            SimpleMarkerStyle.Circle
-                            );
-                        CIMSimpleRenderer renderer = selectionLayer.GetRenderer() as CIMSimpleRenderer;
-                        renderer.Symbol = pointSymbol.MakeSymbolReference();
-                        selectionLayer.SetRenderer(renderer);
-                    }
+                    //    CIMPointSymbol pointSymbol = SymbolFactory.Instance.ConstructPointSymbol(
+                    //        ColorFactory.Instance.GreenRGB,
+                    //        8.0,
+                    //        SimpleMarkerStyle.Circle
+                    //        );
+                    //    CIMSimpleRenderer renderer = selectionLayer.GetRenderer() as CIMSimpleRenderer;
+                    //    renderer.Symbol = pointSymbol.MakeSymbolReference();
+                    //    selectionLayer.SetRenderer(renderer);
+                    //}
                 }
 
                 else
@@ -170,34 +173,35 @@ namespace SewerSelect
                 var linesExists = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().Any(s => s.Name == "Sewer Lines");
                 if (linesExists)
                 {
-                    var lines = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Sewer Lines");
-                    //Get the selected features from the map.
-                    var linesOIDList = lines.GetSelection().GetObjectIDs();
-                    var linesOID = lines.GetTable().GetDefinition().GetObjectIDField();
-                    if (linesOIDList.Count() == 0)
-                    {
-                        MessageBox.Show("There are no sewer lines selected.", "Warning");
-                    }
+                    Module1.MakeLinesLayer(mapView);
+                    //var lines = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Sewer Lines");
+                    ////Get the selected features from the map.
+                    //var linesOIDList = lines.GetSelection().GetObjectIDs();
+                    //var linesOID = lines.GetTable().GetDefinition().GetObjectIDField();
+                    //if (linesOIDList.Count() == 0)
+                    //{
+                    //    MessageBox.Show("There are no sewer lines selected.", "Warning");
+                    //}
 
-                    else
-                    {
-                        string defQuery = $"{linesOID} in ({string.Join(",", linesOIDList)})";
-                        string url = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.SEWERMAN.SEWERS_VIEW";
+                    //else
+                    //{
+                    //    string defQuery = $"{linesOID} in ({string.Join(",", linesOIDList)})";
+                    //    string url = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.SEWERMAN.SEWERS_VIEW";
 
-                        Uri uri = new Uri(url);
-                        var selectionLayer = LayerFactory.Instance.CreateFeatureLayer(uri, mapView, 0, "Sewer Lines SELECTION");
+                    //    Uri uri = new Uri(url);
+                    //    var selectionLayer = LayerFactory.Instance.CreateFeatureLayer(uri, mapView, 0, "Sewer Lines SELECTION");
         
-                        selectionLayer.SetDefinitionQuery(defQuery);
+                    //    selectionLayer.SetDefinitionQuery(defQuery);
 
-                        CIMLineSymbol lineSymbol = SymbolFactory.Instance.ConstructLineSymbol(
-                            ColorFactory.Instance.RedRGB,
-                            3.0,
-                            SimpleLineStyle.Solid
-                            );
-                        CIMSimpleRenderer renderer = selectionLayer.GetRenderer() as CIMSimpleRenderer;
-                        renderer.Symbol = lineSymbol.MakeSymbolReference();
-                        selectionLayer.SetRenderer(renderer);
-                    }
+                    //    CIMLineSymbol lineSymbol = SymbolFactory.Instance.ConstructLineSymbol(
+                    //        ColorFactory.Instance.RedRGB,
+                    //        3.0,
+                    //        SimpleLineStyle.Solid
+                    //        );
+                    //    CIMSimpleRenderer renderer = selectionLayer.GetRenderer() as CIMSimpleRenderer;
+                    //    renderer.Symbol = lineSymbol.MakeSymbolReference();
+                    //    selectionLayer.SetRenderer(renderer);
+                    //}
                 }
 
                 else
--- SewerSelect/Module1.cs
+++ SewerSelect/Module1.cs
@@ -33,32 +33,198 @@ namespace SewerSelect
                 return _this ?? (_this = (Module1)FrameworkApplication.FindModule("SewerSelect_Module"));
             }
         }
-        //public Task MakeLinesLayerAsync()
-        //{
-        //    return QueuedTask.Run(() =>
-        //    {
-        //        //Get the active map view.
-        //        var mapView = MapView.Active.Map;
-        //        if (mapView == null)
-        //            return;
 
-        //        var sewerLayer = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Sewer Lines");
-        //        var selectedFeatures = sewerLayer.GetSelection();
-        //        var count = selectedFeatures.GetCount();
+        public static void MakeSewersLayers(Map mapView)
+        {
+            var mh = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Manholes");
+            var lines = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Sewer Lines");
 
-        //        string caption = "GetSelection()";
-        //        string message = $"Sewers Selected: {count}";
+            //Get the selected features from the map.
+            var mhOIDList = mh.GetSelection().GetObjectIDs();
+            var mhOID = mh.GetTable().GetDefinition().GetObjectIDField();
+            var linesOIDList = lines.GetSelection().GetObjectIDs();
+            var linesOID = lines.GetTable().GetDefinition().GetObjectIDField();
 
-        //        MessageBox.Show(message, caption);
+            if (mhOIDList.Count() == 0 && linesOIDList.Count() == 0)
+            {
+                MessageBox.Show("Manholes & Sewers contain no selected features.", "Warning");
+            }
 
+            else if (mhOIDList.Count() == 0 && linesOIDList.Count() > 0)
+            {
+                MessageBox.Show("Manholes layer contains no selected features.", "Warning");
+            }
 
-        //    });
-        //}
+            else if (mhOIDList.Count() > 0 && linesOIDList.Count() == 0)
+            {
+                MessageBox.Show("Sewer Lines layer contains no selected features.", "Warning");
+            }
+
+            else
+            {
+                string linesDefQuery = $"{linesOID} in ({string.Join(",", linesOIDList)})";
+                string linesURL = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.SEWERMAN.SEWERS_VIEW";
+
+                Uri linesURI = new Uri(linesURL);
+                var linesSelLayer = LayerFactory.Instance.CreateFeatureLayer(linesURI, mapView, 0, "Sewer Lines SELECTION");
+
+                linesSelLayer.SetDefinitionQuery(linesDefQuery);
+
+                CIMLineSymbol lineSymbol = SymbolFactory.Instance.ConstructLineSymbol(
+                    ColorFactory.Instance.RedRGB,
+                    3.0,
+                    SimpleLineStyle.Solid
+                    );
+
+                CIMSimpleRenderer lineRenderer = linesSelLayer.GetRenderer() as CIMSimpleRenderer;
+                lineRenderer.Symbol = lineSymbol.MakeSymbolReference();
+                linesSelLayer.SetRenderer(lineRenderer);
+
+                string mhDefQuery = $"{mhOID} in ({string.Join(",", mhOIDList)})";
+                string mhURL = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.SEWERMAN.MANHOLES_VIEW";
+
+                Uri mhURI = new Uri(mhURL);
+                var mhSelLayer = LayerFactory.Instance.CreateFeatureLayer(mhURI, mapView, 0, "Manholes SELECTION");
+
+                mhSelLayer.SetDefinitionQuery(mhDefQuery);
+
+                CIMPointSymbol pointSymbol = SymbolFactory.Instance.ConstructPointSymbol(
+                    ColorFactory.Instance.GreenRGB,
+                    8.0,
+                    SimpleMarkerStyle.Circle
+                    );
+
+                CIMSimpleRenderer pointRenderer = mhSelLayer.GetRenderer() as CIMSimpleRenderer;
+                pointRenderer.Symbol = pointSymbol.MakeSymbolReference();
+                mhSelLayer.SetRenderer(pointRenderer);
+
+            }
+        }
+
+        public static void MakeManholesLayer(Map mapView)
+        {
+            var mh = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Manholes");
+
+            //Get the selected features from the map.
+            var mhOIDList = mh.GetSelection().GetObjectIDs();
+            var mhOID = mh.GetTable().GetDefinition().GetObjectIDField();
+
+            if (mhOIDList.Count() == 0)
+            {
+                MessageBox.Show("There are no manholes selected.", "Warning");
+            }
+
+            else
+            {
+                string defQuery = $"{mhOID} in ({string.Join(",", mhOIDList)})";
+                string url = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.SEWERMAN.MANHOLES_VIEW";
+
+                Uri uri = new Uri(url);
+                var selectionLayer = LayerFactory.Instance.CreateFeatureLayer(uri, mapView, 0, "Manholes SELECTION");
+
+                selectionLayer.SetDefinitionQuery(defQuery);
+
+                CIMPointSymbol pointSymbol = SymbolFactory.Instance.ConstructPointSymbol(
+                    ColorFactory.Instance.GreenRGB,
+                    8.0,
+                    SimpleMarkerStyle.Circle
+                    );
+                CIMSimpleRenderer renderer = selectionLayer.GetRenderer() as CIMSimpleRenderer;
+                renderer.Symbol = pointSymbol.MakeSymbolReference();
+                selectionLayer.SetRenderer(renderer);
+            }
+
+        }
+
+        public static void MakeLinesLayer(Map mapView)
+        {
+            var lines = mapView.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(s => s.Name == "Sewer Lines");
+            //Get the selected features from the map.
+            var linesOIDList = lines.GetSelection().GetObjectIDs();
+            var linesOID = lines.GetTable().GetDefinition().GetObjectIDField();
+            if (linesOIDList.Count() == 0)
+            {
+                MessageBox.Show("There are no sewer lines selected.", "Warning");
+            }
+
+            else
+            {
+                string defQuery = $"{linesOID} in ({string.Join(",", linesOIDList)})";
+                string url = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\GISVIEWER.SDE@SQL0.sde\SDE.SEWERMAN.SEWERS_VIEW";
+
+                Uri uri = new Uri(url);
+                var selectionLayer = LayerFactory.Instance.CreateFeatureLayer(uri, mapView, 0, "Sewer Lines SELECTION");
+
+                selectionLayer.SetDefinitionQuery(defQuery);
+
+                CIMLineSymbol lineSymbol = SymbolFactory.Instance.ConstructLineSymbol(
+                    ColorFactory.Instance.RedRGB,
+                    3.0,
+                    SimpleLineStyle.Solid
+                    );
+                CIMSimpleRenderer renderer = selectionLayer.GetRenderer() as CIMSimpleRenderer;
+                renderer.Symbol = lineSymbol.MakeSymbolReference();
+                selectionLayer.SetRenderer(renderer);
+            }
+        }
+
+        public static bool ManholesLinesSelected(FeatureLayer mhLayer, FeatureLayer linesLayer)
+        {
+            //Get the selected features from the map.
+            var mhOIDList = mhLayer.GetSelection().GetObjectIDs();
+            var mhOID = mhLayer.GetTable().GetDefinition().GetObjectIDField();
+            var linesOIDList = linesLayer.GetSelection().GetObjectIDs();
+            var linesOID = linesLayer.GetTable().GetDefinition().GetObjectIDField();
+
+            if (mhOIDList.Count() == 0 || linesOIDList.Count() == 0)
+            {
+                MessageBox.Show("Manholes and/or Sewer Lines contain no selected features.", "Warning");
+                return false;
+            }
+
+            else 
+            {
+                return true;
+            }
+        }
+
+        public static bool ManholesSelected(FeatureLayer mhLayer)
+        {
+            var mhOIDList = mhLayer.GetSelection().GetObjectIDs();
+            var linesOID = mhLayer.GetTable().GetDefinition().GetObjectIDField();
+            if (mhOIDList.Count > 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                MessageBox.Show("There are no manholes selected.", "Warning");
+                return false;
+            }
+        }
+
+        public static bool LinesSelected(FeatureLayer linesLayer)
+        {
+            var linesOIDList = linesLayer.GetSelection().GetObjectIDs();
+            var linesOID = linesLayer.GetTable().GetDefinition().GetObjectIDField();
+            if (linesOIDList.Count > 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                MessageBox.Show("There are no sewer lines selected.", "Warning");
+                return false;
+            }
+        }
+
         #region Overrides
-            /// <summary>
-            /// Called by Framework when ArcGIS Pro is closing
-            /// </summary>
-            /// <returns>False to prevent Pro from closing, otherwise True</returns>
+        /// <summary>
+        /// Called by Framework when ArcGIS Pro is closing
+        /// </summary>
+        /// <returns>False to prevent Pro from closing, otherwise True</returns>
         protected override bool CanUnload()
         {
             //TODO - add your business logic
